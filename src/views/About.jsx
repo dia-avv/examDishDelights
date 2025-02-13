18b7c3f377640afd570033acb79e59314f964100
dia-avv <eaa24avv@students.eaaa.dk>
2025-02-13 16:56:41 +0100

    try4

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
             <h1>About Us</h1>
             <h4 style={{marginTop: "0"}}><i>Dish Delights</i> - your go-to destination for discovering and sharing delicious recipes from around the world!</h4>
             <p>We believe that cooking is more than just preparing food—it’s an <i>experience</i>, a way to connect with loved ones, and an opportunity to explore different cultures through flavors and ingredients. Whether you’re a seasoned chef or a kitchen newbie, our platform is designed to inspire your <i>culinary journey</i>.</p>
-            <Link to="/recipes"><button>Explore Recipes</button></Link>
+            <Link to="examDishDelights/recipes"><button>Explore Recipes</button></Link>
             <h3>What We Offer</h3>
             <div className={styles.offersContainer}>
                 <div className={styles.offer}>
@@ -25,7 +25,7 @@ export default function About() {
             </div>
             <h3>Join the Dish Delights Community!</h3>
             <p>We’re more than just a recipe website—we’re a <i>community</i> of food lovers who enjoy experimenting in the kitchen and sharing our passion for great meals. So, <i>start exploring</i>, <i>get cooking</i>, and don’t forget to <i>add your own</i> delightful dishes to inspire others!</p>
-            <Link to="/favorites"><button>Create A Recipe</button></Link>
+            <Link to="examDishDelights/favorites"><button>Create A Recipe</button></Link>
             <h4 style={{marginTop: "40px", color: "coral"}}>Happy Cooking!</h4>
         </div>
     )
